bd89819575917ccbc93eca8ec537b52d1479cd3e
Aldi Maulana <aldimaul07@gmail.com>
2024-05-18 22:21:31 +0700

    chore: Remove unnecessary files and update package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
