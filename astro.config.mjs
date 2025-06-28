71bc19108255118b8d7ebfba75fce8330448e985
Aldi Maulana <aldimaul07@gmail.com>
2025-06-28 09:07:32 +0700

    ganti link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aldimaulana.me/",
+  site: "https://aldimaull.github.io/",
   integrations: [react(), tailwind(), sitemap(), mdx()],
 });
